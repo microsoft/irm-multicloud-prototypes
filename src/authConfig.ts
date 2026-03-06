a62da2d59ad6fc536af7019394c66e128b7e2e38
vamsiinnamuri <vamsi.innamuri@microsoft.com>
2026-03-06 17:20:20 +0530

    Fix navigateToLoginRequestUrl type error in authConfig

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -6,7 +6,6 @@ export const msalConfig: Configuration = {
     clientId: "367b5007-933b-4bb4-9ee8-6838ca51ab6a",
     authority: "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47", // Microsoft tenant
     redirectUri: window.location.origin + "/irm-multicloud-prototypes/",
-    navigateToLoginRequestUrl: true,
   },
   cache: {
     cacheLocation: "localStorage",
